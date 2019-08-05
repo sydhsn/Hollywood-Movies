396a7f671b8d53daeec78ba83bf3d98a4779ea03
Saiyad Husain <saiyad.husain@ymail.com>
2019-08-05 23:54:38 +0530

    Updates

---
+++ precache-manifest.d9f1981d168632877ce79e3ce104f1b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9024c890a12919fda1e20237a6bb7d",
+    "url": "/sydhsn/Hollywood-Movies/index.html"
+  },
+  {
+    "revision": "60c153f49f6e2d407019",
+    "url": "/sydhsn/Hollywood-Movies/static/css/main.effe4879.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/sydhsn/Hollywood-Movies/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "60c153f49f6e2d407019",
+    "url": "/sydhsn/Hollywood-Movies/static/js/main.9bdb68fd.chunk.js"
+  },
+  {
+    "revision": "aacb6f92dd0279b7d662",
+    "url": "/sydhsn/Hollywood-Movies/static/js/runtime~main.9240a476.js"
+  }
+]);
